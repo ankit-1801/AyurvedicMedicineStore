e07d0121c16551c725e527c11eafe83c0b9b5f3b
Ankit <negiankit705@gmail.com>
2022-07-24 14:36:02 +0530

    fifth commit

--- AyurvedaMedicine/src/main/java/com/ayurvedamedicine/controller/OrderController.java
+++ AyurvedaMedicine/src/main/java/com/ayurvedamedicine/controller/OrderController.java
@@ -7,9 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-//import org.springframework.util.StringUtils;
-//
-//import javax.transaction.Transactional;
 import java.io.IOException;
 import java.util.List;
 @CrossOrigin("*")
